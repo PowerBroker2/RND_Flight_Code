7ac92ff0c9eda6840b33846e8149764802529af9
PB2 <gitstuff2@gmail.com>
2019-10-17 19:54:11 -0400

    Deleted commented prints, stopped telem reporting to GS, added control surface position logging

--- Flight_Controller/Flight_Controller.ino
+++ Flight_Controller/Flight_Controller.ino
@@ -3,10 +3,17 @@
 
 
 
+SerialTransfer telemTx;
+
+
+
+
 void setup()
 {
-  //initialize the core flight controller software class
   myIFC.begin();
+  Serial5.begin(2000000);
+
+  telemTx.begin(Serial5);
 }
 
 
@@ -14,48 +21,53 @@ void setup()
 
 void loop()
 {
-  if(myIFC.grabData_GPS())
-  {
-    //optional debugging prints
-    /*IFC_DEBUG_PORT.print("Latitude:\t\t");       IFC_DEBUG_PORT.println(myIFC.telemetry.latitude, 5);
-    IFC_DEBUG_PORT.print("Longitude:\t\t");        IFC_DEBUG_PORT.println(myIFC.telemetry.longitude, 5);
-    IFC_DEBUG_PORT.print("UTC_year:\t\t");         IFC_DEBUG_PORT.println(myIFC.telemetry.UTC_year);
-    IFC_DEBUG_PORT.print("UTC_month:\t\t");        IFC_DEBUG_PORT.println(myIFC.telemetry.UTC_month);
-    IFC_DEBUG_PORT.print("UTC_day:\t\t");          IFC_DEBUG_PORT.println(myIFC.telemetry.UTC_day);
-    IFC_DEBUG_PORT.print("UTC_hour:\t\t");         IFC_DEBUG_PORT.println(myIFC.telemetry.UTC_hour);
-    IFC_DEBUG_PORT.print("UTC_minute:\t\t");       IFC_DEBUG_PORT.println(myIFC.telemetry.UTC_minute);
-    IFC_DEBUG_PORT.print("UTC_second:\t\t");       IFC_DEBUG_PORT.println(myIFC.telemetry.UTC_second);
-    IFC_DEBUG_PORT.print("Speed Over Ground:\t");  IFC_DEBUG_PORT.println(myIFC.telemetry.speedOverGround, 3);
-    IFC_DEBUG_PORT.print("Course Over Ground:\t"); IFC_DEBUG_PORT.println(myIFC.telemetry.courseOverGround, 3);
-    IFC_DEBUG_PORT.println();*/
-  }
-
-  if(myIFC.grabData_IMU())
-  {
-    //optional debugging prints
-    /*IFC_DEBUG_PORT.print("Pitch Angle:\t\t");  IFC_DEBUG_PORT.println(myIFC.telemetry.pitchAngle, 5);
-    IFC_DEBUG_PORT.print("Roll Angle:\t\t");     IFC_DEBUG_PORT.println(myIFC.telemetry.rollAngle, 5);
-    //IFC_DEBUG_PORT.print("Course Angle:\t\t"); IFC_DEBUG_PORT.println(myIFC.telemetry.courseAngle, 5);
-    IFC_DEBUG_PORT.println();*/
-  }
-
-  if(myIFC.grabData_LiDAR())
-  {
-    //optional debugging prints
-    /*IFC_DEBUG_PORT.print("Converted Altitude:\t"); IFC_DEBUG_PORT.println(myIFC.telemetry.convertedAltitude);
-    IFC_DEBUG_PORT.println();*/
-  }
-
-  if(myIFC.grabData_Pitot())
-  {
-    //optional debugging prints
-    /*IFC_DEBUG_PORT.print("Converted Airspeed:\t"); IFC_DEBUG_PORT.println(myIFC.telemetry.velocity);
-    IFC_DEBUG_PORT.println();*/
-  }
-
-  //update all servo positions based on the new data
+  myIFC.grabData_GPS();
+  myIFC.grabData_IMU();
+  myIFC.grabData_LiDAR();
+  myIFC.grabData_Pitot();
   myIFC.updateServos();
+  //myIFC.sendTelem();
 
-  //send the telemetry to the radio
-  myIFC.sendTelem();
+  sendToDatalogger();
+}
+
+
+
+
+void sendToDatalogger()
+{
+  telemTx.txBuff[0]  = (uint8_t)(((int16_t)(myIFC.telemetry.velocity * 100)) >> 8);
+  telemTx.txBuff[1]  = (uint8_t)(((int16_t)(myIFC.telemetry.velocity * 100)) & 0xFF);
+  telemTx.txBuff[2]  = (uint8_t)(((int16_t)(myIFC.telemetry.altitude * 100)) >> 8);
+  telemTx.txBuff[3]  = (uint8_t)(((int16_t)(myIFC.telemetry.altitude * 100)) & 0xFF);
+  telemTx.txBuff[4]  = (uint8_t)(((int16_t)(myIFC.telemetry.pitchAngle * 100)) >> 8);
+  telemTx.txBuff[5]  = (uint8_t)(((int16_t)(myIFC.telemetry.pitchAngle * 100)) & 0xFF);
+  telemTx.txBuff[6]  = (uint8_t)(((int16_t)(myIFC.telemetry.rollAngle * 100)) >> 8);
+  telemTx.txBuff[7]  = (uint8_t)(((int16_t)(myIFC.telemetry.rollAngle * 100)) & 0xFF);
+  telemTx.txBuff[8]  = (uint8_t)(((int16_t)(myIFC.telemetry.latitude * 100)) >> 8);
+  telemTx.txBuff[9]  = (uint8_t)(((int16_t)(myIFC.telemetry.latitude * 100)) & 0xFF);
+  telemTx.txBuff[10] = (uint8_t)(((int16_t)(myIFC.telemetry.longitude * 100)) >> 8);
+  telemTx.txBuff[11] = (uint8_t)(((int16_t)(myIFC.telemetry.longitude * 100)) & 0xFF);
+  telemTx.txBuff[12] = (uint8_t)(myIFC.telemetry.UTC_year >> 8);
+  telemTx.txBuff[13] = (uint8_t)(myIFC.telemetry.UTC_year & 0xFF);
+  telemTx.txBuff[14] = myIFC.telemetry.UTC_month;
+  telemTx.txBuff[15] = myIFC.telemetry.UTC_day;
+  telemTx.txBuff[16] = myIFC.telemetry.UTC_hour;
+  telemTx.txBuff[17] = myIFC.telemetry.UTC_minute;
+  telemTx.txBuff[18] = (uint8_t)(((int16_t)(myIFC.telemetry.UTC_second * 100)) >> 8);
+  telemTx.txBuff[19] = (uint8_t)(((int16_t)(myIFC.telemetry.UTC_second * 100)) & 0xFF);
+  telemTx.txBuff[20] = (uint8_t)(((int16_t)(myIFC.telemetry.speedOverGround * 100)) >> 8);
+  telemTx.txBuff[21] = (uint8_t)(((int16_t)(myIFC.telemetry.speedOverGround * 100)) & 0xFF);
+  telemTx.txBuff[22] = (uint8_t)(((int16_t)(myIFC.telemetry.courseOverGround * 100)) >> 8);
+  telemTx.txBuff[23] = (uint8_t)(((int16_t)(myIFC.telemetry.courseOverGround * 100)) & 0xFF);
+  telemTx.txBuff[24] = (uint8_t)(myIFC.controlInputs.throttle_command >> 8);
+  telemTx.txBuff[25] = (uint8_t)(myIFC.controlInputs.throttle_command & 0xFF);
+  telemTx.txBuff[26] = (uint8_t)(myIFC.controlInputs.pitch_command >> 8);
+  telemTx.txBuff[27] = (uint8_t)(myIFC.controlInputs.pitch_command & 0xFF);
+  telemTx.txBuff[28] = (uint8_t)(myIFC.controlInputs.yaw_command >> 8);
+  telemTx.txBuff[29] = (uint8_t)(myIFC.controlInputs.yaw_command & 0xFF);
+  telemTx.txBuff[30] = (uint8_t)(myIFC.controlInputs.roll_command >> 8);
+  telemTx.txBuff[31] = (uint8_t)(myIFC.controlInputs.roll_command & 0xFF);
+
+  telemTx.sendData(32);
 }
