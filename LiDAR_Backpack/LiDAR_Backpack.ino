ba9bfa6f732168449d279ddc40c19e5fa497ba2f
PB2 <gitstuff2@gmail.com>
2020-04-24 20:45:06 -0400

    Create LiDAR_Backpack.ino

---
+++ LiDAR_Backpack/LiDAR_Backpack.ino
@@ -0,0 +1,32 @@
+#include <LIDARLite.h>
+#include <SerialTransfer.h>
+
+
+LIDARLite myLidarLite;
+SerialTransfer myTransfer;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  myTransfer.begin(Serial);
+  myLidarLite.begin(0, true);
+  
+  myLidarLite.configure(0);
+}
+
+
+void loop()
+{
+  sendDistance(myLidarLite.distance());
+  
+  for(int i = 0; i < 99; i++)
+    sendDistance(myLidarLite.distance(false));
+}
+
+
+void sendDistance(const float &dist)
+{
+  myTransfer.txObj(dist, sizeof(dist));
+  myTransfer.sendData(dist);
+}
