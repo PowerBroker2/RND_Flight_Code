48c8e7f02169ad66b20c6ce3f7049b74c086beeb
PB2 <gitstuff2@gmail.com>
2020-04-25 22:57:20 -0400

    Update Flight_Controller.ino

--- Flight_Controller/Flight_Controller.ino
+++ Flight_Controller/Flight_Controller.ino
@@ -1,14 +1,15 @@
 #include "IFC_Tools.h"
-#include "FireTimer.h"
 
 
 
-
-SerialTransfer telemTransfer;
-FireTimer telemTimer;
 
 const uint16_t TELEM_REPORT_PERIOD_MS = 20;
-unsigned long  previous_time = millis();
+const int16_t  NWLG_OFFSET            = -100;
+
+
+
+
+Servo nwlg;
 
 
 
@@ -16,10 +17,7 @@ unsigned long  previous_time = millis();
 void setup()
 {
   myIFC.begin();
-  Serial5.begin(2000000);
-
-  telemTransfer.begin(Serial5);
-  telemTimer.begin(TELEM_REPORT_PERIOD_MS);
+  nwlg.attach(29);
 }
 
 
@@ -31,26 +29,7 @@ void loop()
   myIFC.grabData_IMU();
   myIFC.grabData_Pitot();
   myIFC.updateServos();
-
-  if(telemTimer.fire())
-  {
-    previous_time += TELEM_REPORT_PERIOD_MS;
-    sendToDatalogger();
-  }
-}
-
-
-
-
-void sendToDatalogger()
-{
-  uint16_t sendLen;
-
-  telemTransfer.txObj(myIFC.telemetry, sizeof(myIFC.telemetry));
-  sendLen = sizeof(myIFC.telemetry);
+  myIFC.sendTelem();
   
-  telemTransfer.txObj(myIFC.controlInputs, sizeof(myIFC.controlInputs), sendLen);
-  sendLen += sizeof(myIFC.controlInputs);
-
-  telemTransfer.sendData(sendLen);
+  nwlg.writeMicroseconds(constrain(map(myIFC.controlInputs.yaw_command, RUDDER_MIN, RUDDER_MAX, RUDDER_MAX, RUDDER_MIN) + NWLG_OFFSET, RUDDER_MIN, RUDDER_MAX));
 }
